ac4d03d444f40e500e7e3f112de1bfd939e112b2
João Trindade <saaspst2006@gmail.com>
2026-02-15 17:24:39 -0300

    feat: post crud

--- src/posts/post.controller.ts
+++ src/posts/post.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import type { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
 import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('post')
 export class PostController {
@@ -15,5 +26,30 @@ export class PostController {
     @Body() postDto: CreatePostDto,
   ) {
     return this.postService.create(req.user.id, postDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async findAllOwned(@Req() req: AuthenticatedRequest) {
+    return this.postService.findAllOwned(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me/:id')
+  async findOneOwned(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', ParseUUIDPipe) id: string,
+  ) {
+    return this.postService.findOneOwnedOrFail({ id }, req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me/:id')
+  async updatePost(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updatedPost: UpdatePostDto,
+  ) {
+    return this.postService.updatePost({ id }, updatedPost, req.user);
   }
 }
--- src/posts/post.service.ts
+++ src/posts/post.service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Post } from './entities/post.entity';
 import { UserService } from 'src/user/user.service';
-import { AuthorResponseDto } from './dto/author-responde.dto';
 import { plainToInstance } from 'class-transformer';
 import { PostResponseDto } from './dto/post-response.dto';
+import { createSlugFromText } from 'src/common/utils/create-slug-from-text';
+import { User } from 'src/user/entities/user.entity';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostService {
+  private readonly logger = new Logger(PostService.name);
+
   constructor(
     @InjectRepository(Post) private readonly postRepository: Repository<Post>,
     private userService: UserService,
@@ -19,14 +28,102 @@ export class PostService {
     const author = await this.userService.findOneByOrFail({ id });
 
     const post = this.postRepository.create({
-      slug: 'asdflbcvcvcvc-' + Math.random().toString(36).substring(2, 8),
+      slug: createSlugFromText(postDto.title),
       title: postDto.title,
       excerpt: postDto.excerpt,
       content: postDto.content,
       author: author,
     });
 
+    const created = await this.postRepository.save(post).catch((e: unknown) => {
+      if (e instanceof Error) {
+        this.logger.error('Erro ao criar post', e.stack);
+      }
+
+      throw new BadRequestException('Erro ao criar o post');
+    });
+
+    return plainToInstance(PostResponseDto, created, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  async findOne(postData: Partial<Post>) {
+    const post = await this.postRepository.findOne({
+      where: postData,
+      relations: ['author'],
+    });
+
+    return post;
+  }
+
+  async findOneOrFail(postData: Partial<Post>) {
+    const post = await this.findOne(postData);
+
+    if (!post) {
+      throw new NotFoundException('Post não encontrado');
+    }
+
+    return post;
+  }
+
+  async findOneOwned(postData: Partial<Post>, author: User) {
+    const post = await this.postRepository.findOne({
+      where: { ...postData, author: { id: author.id } },
+      relations: ['author'],
+    });
+
+    return post;
+  }
+
+  async findOneOwnedOrFail(postData: Partial<Post>, author: User) {
+    const post = await this.getOwnedEntity(postData, author);
+
     return plainToInstance(PostResponseDto, post, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  async getOwnedEntity(postData: Partial<Post>, author: User) {
+    const post = await this.findOneOwned(postData, author);
+
+    if (!post) {
+      throw new NotFoundException('Post não encontrado');
+    }
+
+    return post;
+  }
+
+  async findAllOwned(author: User) {
+    const posts = await this.postRepository.find({
+      where: {
+        author: { id: author.id },
+      },
+      order: { createdAt: 'DESC' },
+      relations: ['author'],
+    });
+
+    return plainToInstance(PostResponseDto, posts, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  async updatePost(
+    postData: Partial<Post>,
+    updatedPost: UpdatePostDto,
+    author: User,
+  ) {
+    const post = await this.getOwnedEntity(postData, author);
+
+    post.title = updatedPost.title ?? post.title;
+    post.content = updatedPost.content ?? post.content;
+    post.excerpt = updatedPost.excerpt ?? post.excerpt;
+    post.coverImageUrl = updatedPost.coverImageUrl ?? post.coverImageUrl;
+    post.published = updatedPost.published ?? post.published;
+
+    const savedPost = await this.postRepository.save(post);
+
+    return plainToInstance(PostResponseDto, savedPost, {
       excludeExtraneousValues: true,
     });
   }
